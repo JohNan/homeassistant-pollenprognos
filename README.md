12906797b52edbd3d0a1234737e66742860674ea
Johan Nenzén <johan.nanzen@gmail.com>
2019-03-29 02:33:52 +0100

    Uses api from Klart.se and renames the component since pollenkoll is no longer used. also splits into sensor per allergen

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
-# home-assistant-pollenkoll
+# Home Assistant Pollennivå
 
-Support for getting current pollen levels from Pollenkollen.se
-Visit https://pollenkoll.se/pollenprognos/ to find available cities
-Visit https://pollenkoll.se/pollenprognos-ostersund/ to find available states
+Support for getting current pollen levels from Klart.se
+Visit https://www.klart.se/se/pollenprognoser/ to find available cities
+
+States
+* 0 = None
+* 1 = Low
+* 2 = Medium
+* 3 = High
+* 4 = Extra high
+
+Place the folder `pollenniva` in `<HA_CONFIG_DIR>/custom_components`
+Add configuration to your `configuration.yaml`
+
+This will create sensors named `senson.pollenniva_CITY_ALLERGEN` and the state will be the current level of that allergen
+
 Example configuration
 
 ```
 sensor:
-  - platform: pollenkoll
+  - platform: pollenniva
+    scan_interval: 4 (default, optional)
     sensors:
       - city: Stockholm
-        state: Gräs
+        allergen: Gräs
+      - city: Stockholm
+        allergen: Hassel
       - city: Östersund
-        state: Hassel
+        allergen: Hassel
 ```
--- pollenniva/sensor.py
+++ pollenniva/sensor.py
@@ -1,17 +1,26 @@
 """
-Support for getting current pollen levels from Pollenkollen.se
-Visit https://pollenkoll.se/pollenprognos/ to find available cities
-Visit https://pollenkoll.se/pollenprognos-ostersund/ to find available states
+Support for getting current pollen levels from Klart.se
+Visit https://www.klart.se/se/pollenprognoser/ to find available cities
+
+States
+0 = None
+1 = Low
+2 = Medium
+3 = High
+4 = Extra high
 
 Example configuration
 
 sensor:
-  - platform: pollenkoll
+  - platform: pollenniva
+    scan_interval: 4 (default, optional)
     sensors:
       - city: Stockholm
-        state: Gräs
+        allergen: Gräs
+      - city: Stockholm
+        allergen: Hassel
       - city: Östersund
-        state: Hassel
+        allergen: Hassel
 """
 
 import logging
@@ -27,28 +36,45 @@ from homeassistant.helpers.entity import Entity
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.components.sensor.rest import RestData
 from homeassistant.const import (CONF_NAME)
+from dateutil import parser
+from datetime import datetime
 
 _LOGGER = logging.getLogger(__name__)
-_ENDPOINT = 'https://pollenkoll.se/wp-content/themes/pollenkoll/api/get_cities.php'
+_ENDPOINT = 'https://api.klart.se/v2/pollen'
 
-ATTR_POLLEN = 'pollen'
-ATTR_TODAY = 'day0_value'
-ATTR_TYPE = 'type'
+STATES = {
+    0: "None",
+    1: "Low",
+    2: "Medium",
+    3: "High",
+    4: "Extra high"
+}
 
-DEFAULT_NAME = 'Pollenkoll'
+STATES_SV = {
+    0: "Inga",
+    1: "Låg",
+    2: "Måttlig",
+    3: "Hög",
+    4: "Extra hög"
+}
+
+DEFAULT_NAME = 'Pollennivå'
+DEFAULT_INTERVAL = 4
 DEFAULT_VERIFY_SSL = True
 CONF_SENSORS = 'sensors'
-
-SCAN_INTERVAL = timedelta(hours=12)
+CONF_INTERVAL = 'scan_interval'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_INTERVAL, default=DEFAULT_INTERVAL): cv.string,
     vol.Required(CONF_SENSORS, default=[]): cv.ensure_list,
 })
 
+SCAN_INTERVAL = timedelta(hours=DEFAULT_INTERVAL)
+
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Set up the PVOutput sensor."""
+    """Set up the Pollen sensor."""
     name = config.get(CONF_NAME)
     sensors = config.get(CONF_SENSORS)
     method = 'GET'
@@ -67,21 +93,21 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     for item in sensors:
         add_devices([PollenkollSensor(rest, name, item)], True)
 
+
 # pylint: disable=no-member
 class PollenkollSensor(Entity):
-    """Representation of a PVOutput sensor."""
+    """Representation of a Pollen sensor."""
 
     def __init__(self, rest, name, item):
-        """Initialize a PVOutput sensor."""
+        """Initialize a Pollen sensor."""
         self._rest = rest
         self._item = item
         self._city = item['city']
         self._state = None
-        self._name = name + " " +item['city']
-        self._pollen = None
+        self._allergen = item['allergen']
+        self._name = name + " " + self._city + " " + self._allergen
+        self._attributes = None
         self._result = None
-        self._status = namedtuple(
-            'status', [ATTR_POLLEN, ATTR_TODAY, ATTR_TYPE])
 
     @property
     def name(self):
@@ -98,8 +124,8 @@ class PollenkollSensor(Entity):
     @property
     def device_state_attributes(self):
         """Return the state attributes of the monitored installation."""
-        if self._pollen is not None:
-            return self._pollen
+        if self._attributes is not None:
+            return self._attributes
 
     def update(self):
         """Get the latest data from the PVOutput API and updates the state."""
@@ -108,16 +134,25 @@ class PollenkollSensor(Entity):
             self._rest.update()
             self._result = json.loads(self._rest.data)
 
-            for item in self._result:
+            for item in self._result['items']:
                 if item['name'] in self._city:
-                    pollen = item['pollen'];
+                    pollen = item['types']
 
-            self._pollen = {}
+            self._attributes = {}
 
             for item in pollen:
-                if item['type'] == self._item['state']:
-                    self._state = item['type'] + ": " + item['day0_value']
-                self._pollen.update({item['type']: item['day0_value']})
+                for level in item['levels']:
+                    dt = parser.parse(level['date'])
+                    date = dt.strftime("%Y-%m-%d")
+                    if date == datetime.now().strftime("%Y-%m-%d"):
+                        if item['name'] == self._allergen:
+                            self._state = level['level']
+                            self._attributes.update({"alergen_name": item['name']})
+                            self._attributes.update({"state_string": STATES.get(self._state)})
+                            self._attributes.update({"state_string_sv": STATES_SV.get(self._state)})
+                            self._attributes.update({"mapUrl": item['mapUrl']})
+                            self._attributes.update({"season_from": item['season']['from']})
+                            self._attributes.update({"season_to": item['season']['to']})
 
         except TypeError as e:
             self._result = None
